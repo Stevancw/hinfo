ec056f1acdde3df3ee0cbb32b061f48aa95a949d
Ed Wallitt <edwallitt@me.com>
2012-02-13 14:09:35 +0000

    added accordion to topic list and further validation on links

--- hinfo/app/models/link.rb
+++ hinfo/app/models/link.rb
@@ -1,7 +1,10 @@
 class Link < ActiveRecord::Base
   
-  # It should have a path
-  validates :path, :presence => true
+  # It should have a path and a name
+  validates :path, :name, :presence => true
+
+  # the path should be unique
+  validates :path, :uniqueness => true
 
   # links belong to a topic
   belongs_to :topic
