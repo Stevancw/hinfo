404aa8f36393313138a905581920f42f82c6b260
Stevan C Wing <stevancw@gmail.com>
2012-02-13 11:31:26 +0000

    init rails

---
+++ hinfo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinfo::Application.config.secret_token = '70fcd8f4976b8c9384b914b864f7c06dd58c1c584a5248ff4b5d8de864a7395a7e1ea8898234928b97f0f67620be279ad5fca1abcbcc1b9e090cbb1226b62e1e'
