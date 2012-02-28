977a69a2ba09d46e73934c4fce1d926d2e854364
Stevan C Wing <stevancw@gmail.com>
2012-02-28 07:49:01 +0000

    Some changes that get Facebook authentication to work

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+	OmniAuth.config.full_host = "http://localhost:3000/"
   provider :twitter, 'P9ekJ77axIGYKxYaxSYw', '61nvQTuTGpuZgYFlae30L79qo6rf23BPaxBaUZq78Y'
   provider :facebook, '366917943326034', '63bfd81b15dbf4dc8f5518a492cdf742'
 end
