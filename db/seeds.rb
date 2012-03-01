1998f74da5bc49dfd88db5e952fefe2e04681849
Stevan C Wing <stevancw@gmail.com>
2012-03-01 08:47:30 +0000

    Added some of the CMT curriculum to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,42 @@
 
 # 1. Seed the topics
 
-#topics = Topic.create([
-#  { :name => "Atrial fibrillaiton" },
-#  { :name => "Heart failure" },
-#  { :name => "Hypertension" },
-#  { :name => "Aortic stenosis" }
-#  ])
-#
-# Seed the links
-#
-#links = Link.create([
-#	{ :name => "Google", :path => "www.google.com", :topic_id => 1 }
-#])
+=begin
+topics = Topic.create([
+ { :name => "Atrial fibrillaiton" },
+ { :name => "Heart failure" },
+ { :name => "Hypertension" },
+ { :name => "Aortic stenosis" }
+ ])
+
+# Core Medical Training curriculum 2011
+
+topics = Topic.create([
+	{ name: "Cardio-Respiratory Arrest"}
+	{ name: "Shocked patient"}
+	{ name: "Unconscious Patient"}
+ 	{ name: "Anaphylaxis"}
+ 	{ name: "Abdominal pain"}
+ 	{ name: "Acute back pain"}
+ 	{ name: "Blackout / Collapse"}
+ 	{ name: "Breathlessness"}
+ 	{ name: "Chest pain"}
+ 	{ name: "Confusion (acute) / Delirium"}
+ 	{ name: "Cough"}
+ 	{ name: "Diarrhoea"}
+ 	{ name: "Falls"}
+ 	{ name: "Fever"}
+ 	{ name: "Fits / Seizure"}
+ 	{ name: "Haematemesis & Melaena"}
+ 	{ name: "Jaundice"}
+ 	{ name: "Limb Pain & Swelling"}
+ 	{ name: "Palpitations"}
+ 	{ name: "Poisoning"}
+ 	{ name: "Rash"}
+ 	{ name: "Vomiting & Nausea"}
+ 	{ name: "Weakness & Paralysis"}
+	])=end
+
+
+
+
