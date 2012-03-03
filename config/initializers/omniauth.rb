3e997cc6aef323a007f99810e89bd60cc06606ee
Stevan Wing <stevancw@me.com>
2012-03-03 20:08:27 +0000

    Changed the initializer omniauth.rb to have different ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,13 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-	OmniAuth.config.full_host = "http://localhost:3000"
-  provider :twitter, 'P9ekJ77axIGYKxYaxSYw', '61nvQTuTGpuZgYFlae30L79qo6rf23BPaxBaUZq78Y'
-  provider :facebook, '366917943326034', '63bfd81b15dbf4dc8f5518a492cdf742'
+if Rails.env == "production"
+	Rails.application.config.middleware.use OmniAuth::Builder do
+		OmniAuth.config.full_host = "http://cthealth.herokuapp.com"
+	  provider :twitter, 'P9ekJ77axIGYKxYaxSYw', '61nvQTuTGpuZgYFlae30L79qo6rf23BPaxBaUZq78Y'
+	  provider :facebook, '366917943326034', '63bfd81b15dbf4dc8f5518a492cdf742'
+	end
+else
+	Rails.application.config.middleware.use OmniAuth::Builder do
+		OmniAuth.config.full_host = "http://localhost:3000"
+	  provider :twitter, 'P9ekJ77axIGYKxYaxSYw', '61nvQTuTGpuZgYFlae30L79qo6rf23BPaxBaUZq78Y'
+	  provider :facebook, '366917943326034', '63bfd81b15dbf4dc8f5518a492cdf742'
+	end
 end
