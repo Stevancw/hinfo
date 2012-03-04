aa367997964b3f48968cb46dc889bb5c12ece988
Stevan Wing <stevancw@me.com>
2012-03-04 20:03:37 +0000

    Added social media icons to login_links partial and some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,42 +7,41 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # 1. Seed the topics
+	topics = Topic.create([
+	 		{ :name => "Atrial fibrillaiton" },
+		 	{ :name => "Heart failure" },
+		 	{ :name => "Hypertension" },
+		 	{ :name => "Aortic stenosis" },
+		])
 
-
-topics = Topic.create([
-	 { :name => "Atrial fibrillaiton" },
-	 { :name => "Heart failure" },
-	 { :name => "Hypertension" },
-	 { :name => "Aortic stenosis" }
- ])
 
 # Core Medical Training curriculum 2011
 
-# topics = Topic.create([
-# 	{ name: "Cardio-Respiratory Arrest"}
-# 	{ name: "Shocked patient"}
-# 	{ name: "Unconscious Patient"}
-#  	{ name: "Anaphylaxis"}
-#  	{ name: "Abdominal pain"}
-#  	{ name: "Acute back pain"}
-#  	{ name: "Blackout / Collapse"}
-#  	{ name: "Breathlessness"}
-#  	{ name: "Chest pain"}
-#  	{ name: "Confusion (acute) / Delirium"}
-#  	{ name: "Cough"}
-#  	{ name: "Diarrhoea"}
-#  	{ name: "Falls"}
-#  	{ name: "Fever"}
-#  	{ name: "Fits / Seizure"}
-#  	{ name: "Haematemesis & Melaena"}
-#  	{ name: "Jaundice"}
-#  	{ name: "Limb Pain & Swelling"}
-#  	{ name: "Palpitations"}
-#  	{ name: "Poisoning"}
-#  	{ name: "Rash"}
-#  	{ name: "Vomiting & Nausea"}
-#  	{ name: "Weakness & Paralysis"}
-# 	])
+ topics = Topic.create([
+ 		{ :name => "Cardio-Respiratory Arrest"},
+ 		{ :name => "Shocked patient"},
+ 		{ :name => "Unconscious Patient"},
+  	{ :name => "Anaphylaxis"},
+  	{ :name => "Abdominal pain"},
+  	{ :name => "Acute back pain"},
+  	{ :name => "Blackout / Collapse"},
+  	{ :name => "Breathlessness"},
+  	{ :name => "Chest pain"},
+  	{ :name => "Confusion (acute) / Delirium"},
+  	{ :name => "Cough"},
+  	{ :name => "Diarrhoea"},
+  	{ :name => "Falls"},
+  	{ :name => "Fever"},
+  	{ :name => "Fits / Seizure"},
+  	{ :name => "Haematemesis & Melaena"},
+  	{ :name => "Jaundice"},
+  	{ :name => "Limb Pain & Swelling"},
+  	{ :name => "Palpitations"},
+  	{ :name => "Poisoning"},
+  	{ :name => "Rash"},
+  	{ :name => "Vomiting & Nausea"},
+  	{ :name => "Weakness & Paralysis"},
+ 	])
 
 
 
